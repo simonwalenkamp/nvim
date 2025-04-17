fe0224ee7a30513794ed6657a90db23f148c394a
Simon Walenkamp <simonwalenkamp@hotmail.com>
2025-04-17 19:53:15 +0200

    Added custom theme

--- init.lua
+++ init.lua
@@ -4,24 +4,15 @@ if vim.g.vscode then
 else
   -- Update PATH to include npm global binaries
   vim.env.PATH = vim.env.PATH .. ';C:\\Users\\Simon\\AppData\\Roaming\\npm'
-
-  -- Set <space> as the leader key
-  -- See `:help mapleader`
-  --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
   vim.g.mapleader = ' '
   vim.g.maplocalleader = ' '
 
-  -- Set to true if you have a Nerd Font installed and selected in the terminal
   vim.g.have_nerd_font = true
 
-  -- [[ Setting options ]]
-  -- See `:help vim.opt`
-  -- NOTE: You can change these options as you wish!
-  --  For more options, you can see `:help option-list`
-
-  -- Make line numbers default
   vim.opt.number = true
   vim.opt.relativenumber = true
+  vim.opt.scrolloff = 13
+  vim.opt.cursorline = true
 
   -- Enable mouse mode, can be useful for resizing splits for example!
   vim.opt.mouse = 'a'
@@ -68,12 +59,6 @@ else
 
   -- Preview substitutions live, as you type!
   vim.opt.inccommand = 'split'
-
-  -- Show which line your cursor is on
-  vim.opt.cursorline = true
-
-  -- Minimal number of screen lines to keep above and below the cursor.
-  vim.opt.scrolloff = 13
 
   -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
   -- instead raise a dialog asking if you wish to save the current file(s)
@@ -790,28 +775,6 @@ else
         -- Shows a signature help window while you type arguments for a function
         signature = { enabled = true },
       },
-    },
-
-    { -- You can easily change to a different colorscheme.
-      -- Change the name of the colorscheme plugin below, and then
-      -- change the command in the config to whatever the name of that colorscheme is.
-      --
-      -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-      'folke/tokyonight.nvim',
-      priority = 1000, -- Make sure to load this before all the other start plugins.
-      config = function()
-        ---@diagnostic disable-next-line: missing-fields
-        require('tokyonight').setup {
-          styles = {
-            comments = { italic = false }, -- Disable italics in comments
-          },
-        }
-
-        -- Load the colorscheme here.
-        -- Like many other themes, this one has different styles, and you could load
-        -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-        vim.cmd.colorscheme 'tokyonight-night'
-      end,
     },
 
     -- Highlight todo, notes, etc in comments
