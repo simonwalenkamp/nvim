99b74684564c111e83d655bea4a3c733a57f5220
Simon Walenkamp <simon.walenkamp@lego.com>
2025-10-31 09:57:36 +0100

    move basic configurations into sepparate file

--- lua/walenkamp/init.lua
+++ lua/walenkamp/init.lua
@@ -1,23 +1,4 @@
 require("walenkamp.remap")
 require("walenkamp.lazy")
-
--- When to start scrolling
-vim.opt.scrolloff = 13
-
--- Line numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
-
--- Use system clipboard
-vim.opt.clipboard = "unnamedplus"
-
--- Highlight on yank
-vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank({
-      higroup = "Visual",  -- highlight group to use
-      timeout = 200,       -- duration (ms)
-    })
-  end,
-})
+require("walenkamp.set")
 
--- lua/walenkamp/plugins/nvim-lspconfig.lua
+++ lua/walenkamp/plugins/nvim-lspconfig.lua
@@ -168,7 +168,12 @@ return {
               callSnippet = 'Replace',
             },
             -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-            -- diagnostics = { disable = { 'missing-fields' } },
+            diagnostics = {
+                disable = { 'missing-fields' },
+                globals = {
+                    'vim'
+                }
+            },
           },
         },
       },
