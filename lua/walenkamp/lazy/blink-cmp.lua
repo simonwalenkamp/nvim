aec3d98713d09b3e815e7b324adfeaac1d12ea94
Simon Walenkamp <simon.walenkamp@lego.com>
2025-10-31 10:55:02 +0100

    move blink-cmp configuration to its own file

---
+++ lua/walenkamp/lazy/blink-cmp.lua
@@ -0,0 +1,25 @@
+return {
+    "saghen/blink.cmp",
+    dependencies = { 'rafamadriz/friendly-snippets' },
+    version = '1.*',
+    opts = {
+        keymap = {
+        preset = 'default',
+        ['<Tab>'] = { 'select_and_accept', 'fallback' },
+      },
+      sources = {
+        -- add lazydev to your completion providers
+        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            -- make lazydev completions top priority (see `:h blink.cmp`)
+            score_offset = 100,
+          },
+        },
+      },
+    fuzzy = { implementation = "prefer_rust_with_warning" }
+    },
+}
+
--- lua/walenkamp/lazy/lazydev.lua
+++ lua/walenkamp/lazy/lazydev.lua
@@ -20,24 +20,4 @@ return {
       })
     end,
   },
-  { -- optional blink completion source for require statements and module annotations
-    "saghen/blink.cmp",
-    dependencies = { 'rafamadriz/friendly-snippets' },
-    version = '1.*',
-    opts = {
-      sources = {
-        -- add lazydev to your completion providers
-        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-        providers = {
-          lazydev = {
-            name = "LazyDev",
-            module = "lazydev.integrations.blink",
-            -- make lazydev completions top priority (see `:h blink.cmp`)
-            score_offset = 100,
-          },
-        },
-      },
-    fuzzy = { implementation = "prefer_rust_with_warning" }
-    },
-  }
 }
