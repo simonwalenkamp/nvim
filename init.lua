064a1a1bab73a69ec236c33fe8db55dab9d4dc7a
Simon Walenkamp <simon.walenkamp@lego.com>
2025-10-31 12:02:31 +0100

    use neo-tree

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
 require("walenkamp")
 
 vim.cmd[[colorscheme tokyonight]]
-
--- lua/walenkamp/remap.lua
+++ lua/walenkamp/remap.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
