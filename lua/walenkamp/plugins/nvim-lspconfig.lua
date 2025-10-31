fe5f1282091ad794885edb79719e4d0c4db01cf1
Simon Walenkamp <simon.walenkamp@lego.com>
2025-10-31 10:16:15 +0100

    add lazydev for helping with building this config

--- lua/walenkamp/plugins/nvim-lspconfig.lua
+++ lua/walenkamp/plugins/nvim-lspconfig.lua
@@ -168,12 +168,7 @@ return {
               callSnippet = 'Replace',
             },
             -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-            diagnostics = {
-                disable = { 'missing-fields' },
-                globals = {
-                    'vim'
-                }
-            },
+            -- diagnostics = { disable = { 'missing-fields' }
           },
         },
       },
