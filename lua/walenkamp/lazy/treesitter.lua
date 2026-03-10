d2ad2c8627534025b76cc214ad4692432c1720b0
Simon Walenkamp <simon.walenkamp@lego.com>
2026-03-10 08:02:55 +0100

    add terraform support.

--- lua/walenkamp/lazy/treesitter.lua
+++ lua/walenkamp/lazy/treesitter.lua
@@ -2,7 +2,7 @@ return { "nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, bui
     config = function()
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-            ensure_installed = {  "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "javascript", "typescript", "scala" },
+            ensure_installed = {  "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "javascript", "typescript", "scala", "hcl", "terraform" },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
