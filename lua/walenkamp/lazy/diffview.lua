f28d344bf5784fdb5d0e31b466129ac8376e842e
Simon Walenkamp <simon.walenkamp@lego.com>
2026-03-14 20:41:19 +0100

    Clean up old plugins.

--- lua/walenkamp/lazy/diffview.lua
+++ lua/walenkamp/lazy/diffview.lua
@@ -1,14 +1,14 @@
 return {
-	"sindrets/diffview.nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	cmd = { "DiffviewOpen", "DiffviewFileHistory", "DiffviewClose" },
-	keys = {
-		{ "<leader>dv", "<cmd>DiffviewOpen<cr>", desc = "Diffview: open diff view" },
-		{ "<leader>dh", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview: file history (current file)" },
-		{ "<leader>dH", "<cmd>DiffviewFileHistory<cr>", desc = "Diffview: file history (branch)" },
-		{ "<leader>dq", "<cmd>DiffviewClose<cr>", desc = "Diffview: close" },
-	},
-	opts = {},
+    "sindrets/diffview.nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    cmd = { "DiffviewOpen", "DiffviewFileHistory", "DiffviewClose" },
+    keys = {
+        { "<leader>dv", "<cmd>DiffviewOpen<cr>",          desc = "Diffview: open diff view" },
+        { "<leader>dh", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview: file history (current file)" },
+        { "<leader>dH", "<cmd>DiffviewFileHistory<cr>",   desc = "Diffview: file history (branch)" },
+        { "<leader>dq", "<cmd>DiffviewClose<cr>",         desc = "Diffview: close" },
+    },
+    opts = {},
 }
