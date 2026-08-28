5f9a59617e374579bbb1f55ca11a34c0e2e9e1c3
Simon Walenkamp <simon.walenkamp@lego.com>
2026-08-28 14:20:20 +0200

    Draw a line where our formatting usually cut of in scala

--- lua/walenkamp/set.lua
+++ lua/walenkamp/set.lua
@@ -29,7 +29,7 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 -- I did not like this column being drawn
--- vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "120"
 
 vim.opt.clipboard = "unnamedplus"
 
