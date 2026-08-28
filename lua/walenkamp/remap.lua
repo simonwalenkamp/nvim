9eab126f50f2195e30c9c2c16ab9951936cc62c3
Simon Walenkamp <simon.walenkamp@lego.com>
2026-08-28 08:33:24 +0200

    More helpfull shortcuts.

--- lua/walenkamp/remap.lua
+++ lua/walenkamp/remap.lua
@@ -5,3 +5,21 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("n", "]d", function()
+  vim.diagnostic.jump({ count = 1, float = true })
+end, { desc = "Next diagnostic" })
+vim.keymap.set("n", "[d", function()
+  vim.diagnostic.jump({ count = -1, float = true })
+end, { desc = "Previous diagnostic" })
+vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line diagnostics" })
+vim.keymap.set("n", "<leader>cq", vim.diagnostic.setloclist, { desc = "Buffer diagnostics" })
+
+local project = require("walenkamp.project")
+
+vim.keymap.set("n", "<leader>tt", function()
+  project.terminal()
+end, { desc = "Terminal (project root)" })
+vim.keymap.set("n", "<leader>gg", function()
+  project.terminal({ "lazygit" })
+end, { desc = "Lazygit (project root)" })
